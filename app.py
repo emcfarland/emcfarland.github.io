e939b3b906cda7e321a3e302861b13206adce8c2
unknown <elliottmcf@gmail.com>
2021-03-06 17:35:24 -0500

    Creates flask app, adds resume json

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify, render_template, redirect, request, session
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    print("Responding to the index page request...")
+    return render_template("index.html")
+
+@app.route("/projects")
+def project():
+    print("Responding to the project page request...")
